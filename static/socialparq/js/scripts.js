1391d36a82ccf3d5a573e2115347489544258c78
Israel Gómez <imarban22@gmail.com>
2015-03-08 06:58:30 -0600

    Se crea métodos para dibujar elementos

--- static/socialparq/js/scripts.js
+++ static/socialparq/js/scripts.js
@@ -5,36 +5,36 @@ var pasos = $('section').length;
 
 
   function prender_menu(){
-		//$("#menu-lateral").toggle();	
+		//$("#menu-lateral").toggle();
 		$("#menu-lateral").animate({left: '0em'}, 500, function() { $(this).show();} );
 	}
- 
+
   function apagar_menu(){
 		$("#menu-lateral").animate({left: '-22em'}, 500, function() { $(this).show();} );
 	}
 
   function apagamos(salida){
-		//COMIENZA CON EL DOS PORQUE QUEREMOS APAGAR LAS SECCIONES DE ABAJO 
+		//COMIENZA CON EL DOS PORQUE QUEREMOS APAGAR LAS SECCIONES DE ABAJO
 		for(x=salida;x>=1;x--){
 			$(".paso"+x).hide();
-		}		
+		}
 
     //APAGAMOS LAS DE ARRIBA
 		for(x=salida+1;x<=pasos;x++){
 			$(".paso"+x).hide();
-		}		
+		}
 	}
 
-  function prendemos(entrada){	
+  function prendemos(entrada){
 		//VAMOS A PRENDER LA SECCION ELEGIDA
 		$(".paso"+entrada).show();
-	} 
+	}
 
 	function iniciar(){
 		//cambiamos el color de fondo por el azul
 		$(".global-container").css("background-color", "#00a0dd");
 		//VAMOS A ESCONDER LAS SECCIONES QUE NO NECESITAMOS
-		//PRENDEMOS EL PASO 1  
+		//PRENDEMOS EL PASO 1
 		apagamos(1);
     prendemos(2);
 	}//FINAL DE INICIAR
@@ -47,10 +47,10 @@ var pasos = $('section').length;
 	function login(){
 	  apagamos(3);
 		prendemos(4);
-	} 
+	}
 
   //FUNCION PARA PRENDER O APAGAR EL MAPA
-	function mapa(){	
+	function mapa(){
 	  apagamos(4);
 		prendemos(5);
 		//FUNCIONES PARA INICIALIZAR EL GOOGLE MAPS, DARLE ESTILOS Y QUE NO SEA ANTES DE ESTO
@@ -58,7 +58,7 @@ var pasos = $('section').length;
 		$(".global-container").css("padding-bottom","0px");
 		$("html, body, #map-canvas, .map").css({height: "100%", margin: "0", padding: "0" });
 	}
-  
+
 	//boton de registro
 	$(".btn-registrarse").click(function() {
     registro();
@@ -81,12 +81,12 @@ var pasos = $('section').length;
 	$(".btn-cerrar").click(function(){
 		apagar_menu();
 	});
-  
+
 	//PARA CERRAR EL MODAL
 	$(".modals").click(function(){
     $(this).hide();
 		$(".overlay").hide();
-	});	
+	});
 
 
 //ES PARA INICIAR LA APLICACIÓN
@@ -105,9 +105,9 @@ function modal_gracias(){
 //FUNCION PARA CENTRAR EL CONTENIDO
 jQuery.fn.center = function () {
 	this.css("position","absolute");
-	this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+	this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
 	$(window).scrollTop()) + "px");
-	this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+	this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
 	$(window).scrollLeft()) + "px");
 	return this;
 }
